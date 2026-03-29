8850c4c570af2a45aa648836655ab84a09206230
Kennysup1 <your-github-email@gmail.com>
2026-03-29 12:53:49 +1200

    Clean up unnamed files

--- Kenny_ML.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import sklearn
-
-print("numpy:", np.__version__)
-print("pandas:", pd.__version__)
-print("sklearn:", sklearn.__version__)
-print("All packages working! ✅")
